bc87b619da256b072c25dd21048e0e019e2d628e
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-06 13:34:04 +0200

    Testing edge cases.

--- src/test/java/TripCalculatorTest.java
+++ src/test/java/TripCalculatorTest.java
@@ -1,13 +1,29 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TripCalculatorTest {
     private TripCalculator calculator;
     @BeforeEach
     public void setUp() {
         calculator = new TripCalculator();
+    }
+    @Test
+    public void testSetDistance(){
+        calculator.setDistanceKm(20);
+        assertEquals(20, calculator.getDistanceKm());
+
+        calculator.setDistanceKm(-20);
+        assertNotEquals(-20, calculator.getDistanceKm());
+    }
+    @Test
+    public void testSetFuelPrice(){
+        calculator.setFuelPricePerLiter(20);
+        assertEquals(20, calculator.getFuelPricePerLiter());
+
+        calculator.setFuelPricePerLiter(-20);
+        assertNotEquals(-20, calculator.getFuelPricePerLiter());
     }
     @Test
     public void testCalculateTrip() {
