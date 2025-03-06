cd262b89caf1aff64549a405a57e4b21b282e7c0
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-06 12:54:42 +0200

    Creating base of TripCalculator

---
+++ src/main/java/TripCalculator.java
@@ -0,0 +1,24 @@
+public class TripCalculator {
+    private int distanceKm;
+    private int fuelPricePerLiter;
+    private final int fuelEfficiency = 20;
+    public TripCalculator(){
+
+    }
+    public void setDistanceKm(int distanceKm){
+        this.distanceKm = distanceKm;
+    }
+    public void setFuelPricePerLiter(int fuelPricePerLiter){
+        this.fuelPricePerLiter = fuelPricePerLiter;
+    }
+    public int getDistanceKm(){
+        return distanceKm;
+    }
+    public int getFuelPricePerLiter(){
+        return fuelPricePerLiter;
+    }
+    public float getPricePerTrip(){
+
+    }
+
+}
